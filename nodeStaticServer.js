884d1970e355ddd39aa3146d229d7228b9f20de2
cflid <986670019@qq.com>
2020-04-22 16:42:03 +0800

    node静态服务器

--- nodeStaticServer.js
+++ nodeStaticServer.js
@@ -4,17 +4,18 @@ var path = require('path')
 var url = require('url')
 
 http.createServer(function (req, res) {
-
-  let mimeJosn = fs.readFileSync(__dirname + '/mime.json');
+  let mimeJosn = fs.readFileSync(__dirname + '/mime.json')
   mimeJosn = JSON.parse(mimeJosn.toString())
+  req.url = req.url === '/' ? '/index.html' : req.url
   let mime = mimeJosn[path.extname(req.url)] 
-  res.writeHead(200, {'Content-Type': mime });
+  res.writeHead(200, {'Content-Type': mime })
+  console.log(req.url)
   if ('/favicon.ico' !== req.url) {
     let content =  fs.readFileSync(__dirname + '/moblie' + req.url)
-      res.write(content.toString())
+    res.write(content.toString())
   }
 
   res.end()
-}).listen(3000);
+}).listen(3300);
 
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://127.0.0.1:3300');
