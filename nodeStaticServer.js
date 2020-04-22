ab4c78236a61cd496c92fa2e825c772092073b20
v_elevennli <986670019@qq.com>
2020-04-22 19:15:38 +0800

    控制台统计每个页面请求时间

--- nodeStaticServer.js
+++ nodeStaticServer.js
@@ -1,22 +1,29 @@
-var http = require('http')
-var fs = require('fs')
-var path = require('path')
-var url = require('url')
-
-http.createServer(function (req, res) {
-  console.time('start')  
-  let mimeJosn = fs.readFileSync(__dirname + '/mime.json')
-  mimeJosn = JSON.parse(mimeJosn.toString())
-  req.url = req.url === '/' ? '/index.html' : req.url
-  let mime = mimeJosn[path.extname(req.url)] 
-  res.writeHead(200, {'Content-Type': mime })
-//   console.log(req.url)
-  if ('/favicon.ico' !== req.url) {
-    let content =  fs.readFileSync(__dirname + '/moblie' + req.url)
-    res.write(content.toString())
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const guid = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
   }
-  console.time('start')
-  res.end()
-}).listen(3300);
+http
+  .createServer(function (req, res) {
+    req.url = req.url === "/" ? "/index.html" : req.url;
+    console.time(req.url);
+    let mimeJosn = fs.readFileSync(__dirname + "/mime.json");
+    mimeJosn = JSON.parse(mimeJosn.toString());
+    let mime = mimeJosn[path.extname(req.url)];
+    res.writeHead(200, { "Content-Type": mime });
+    //   console.log(req.url)
+    if ("/favicon.ico" !== req.url) {
+      let content = fs.readFileSync(__dirname + "/moblie" + req.url);
+      res.write(content.toString());
+    }
+    console.timeEnd(req.url);
+    res.end();
+  })
+  .listen(3300);
 
-console.log('Server running at http://127.0.0.1:3300');
+console.log("Server running at http://127.0.0.1:3300");
